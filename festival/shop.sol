bf8ca614da11202b91cfd3fdea12e04b0d6c4e90
Concept <conceptcodes@gmail.com>
2021-12-20 14:52:25 -0500

    add final touches to shop contradct, will need to refactor heavily

--- festival/shop.sol
+++ festival/shop.sol
@@ -27,22 +27,18 @@ contract Shop {
         _ogTicketPrice = ticket.getOriginalPrice();
     }
 
-    function _buyTicket(address _guest) public returns(uint256) {
+    function buyTicket() public returns(uint256) {
         require(_ticket.ticketsAvailable());
         if(!_token.transferFrom(msg.sender, address(this), _ogTicketPrice))) {
             revert();
         } 
-        uint256 tokenId = _ticket.mint(_guest)
+        uint256 tokenId = _ticket.mint(msg.sender)
         _ticketInfo[tokenId] = TicketInfo({
             lastSellPrice: _ogTicketPrice,
             salePrice: 0
         });
         _tokenIds.push(tokenId);
         return tokenId;
-    }
-
-    function buyTicket() public returns(uint256) {
-        return _buyTicket(msg.sender);
     }
 
     function _resaleTicket(uint256 tokenId, uint256 price) public {
@@ -52,6 +48,36 @@ contract Shop {
         _ticketInfo[tokenId].salePrice = price;
     }
 
-    function resaleTicket()
+    function resaleTicket(uint256 tokenId, address _guest) public {  
+        require(_ticketInfo[tokenId].salePrice != uint256(0), "Ticket must be for sale");
+        address owner = ticket.ownerOf(tokenId);
+        uint256 price = _ticketInfo[tokenId].salePrice;
+        require(_token.allowance(msg.sender, address(this)) >= price, "");
+        if(!(_token.transferFrom(msg.sender, address(this), price * commision / 100))) {
+            revert();
+        }
+        if(!(_token.transferFrom(msg.sender, owner, price * (100 - commision) / 100))) {
+            revert();
+        }
+        _ticket.safeTransferFrom(owner, _guest, tokenId);
+        _ticketInfo[tokenId].lastSellPrice = _ticketInfo[tokenId].salePrice;
+        _ticketInfo[tokenId].salePrice = 0;
+    } 
+
+    function getOriginalPrice() external view returns(uint256) {
+        return _ogTicketPrice;
+    }
+
+    function getTicketsSold() external view returns(uint256[] memory) {
+        return _tokenIds;
+    }
+
+    function getLastSellPrice(uint256 _id) external view returns(uint256) {
+        return _ticketInfo[_id].lastSellPrice;
+    }
+
+    function getForSalePrice(uint256 _id) external view returns(uint256) {
+        return _ticketInfo[_id].salePrice;
+    }
 }
 
