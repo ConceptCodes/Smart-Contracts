af0cb329c8142f116af1ff2fc735ac1fee115fc6
Concept <conceptcodes@gmail.com>
2021-12-27 12:09:32 -0500

    added more juice, will test in remix shortly

--- HQ.sol
+++ HQ.sol
@@ -8,7 +8,7 @@ import "@openzeppelin/contracts/ownership/Ownable.sol";
 /// @author concept
 /// @notice allows you to answer questions to win money
 contract HQ is Ownable {
-    mapping(string => Question) questions;
+    mapping(uint256 => Question) questions;
     
     struct Question {
         string prompt;
@@ -16,26 +16,37 @@ contract HQ is Ownable {
         bool correct;
     }
 
-    constructor() Ownable() { 
-        // load in the questions and hash the answers
+    event questionAnswered(bool guess);
+
+    constructor(string[] _questions, string[] _answers) Ownable() { 
+        for(uint256 i = 0, i <= _questions.length; i++) {
+            for(uint256 j = 0, j <= _answers.length; j++) {
+                questions[i] = Question({
+                    prompt: _questions[i],
+                    answer: _answers[j]
+                    correct: false
+                })
+            }
+        }
     }
     
     /// @notice fund the contract with ether
     /// @dev only the owner can send ether to the contract
-    receive() external payable  onlyOwner {
-        
+    receive() external payable onlyOwner {
+        require(msg.value >= 1 ether, "Minimum amount is 1 ether");
     }
 
-    /// @notice how you answer the quesstion
+    /// @notice how you answer the question
     /// @dev checks if your guess properly matches the question's answer
     /// @param _answer your answer to the question
     /// @param _question the question your curretly ansewring
-    /// @return true if answer is correct
-    function guess(string memory _question, string memory _answer) public view returns (bool) {
+    function guess(string memory _question, string memory _answer) external view  {
         if(keccak256(abi.encodePacked(questions[_question].answer)) == _answer){
             questions[_question].correct = true;
+            emit questionAnswered(true);
         } else{
-            
+            emit questionAnswered(false);
+            revert("Sorry your answer was incorrect")
         }
     }
 
