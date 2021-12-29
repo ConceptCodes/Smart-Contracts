506078a596358bd4078faa7d778fca41aede10c2
Concept <conceptcodes@gmail.com>
2021-12-28 21:01:13 -0500

    cleaner i think

--- HQ.sol
+++ HQ.sol
@@ -2,19 +2,19 @@
 
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/ownership/Ownable.sol";
+import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol";
 
 /// @title HQ trivia game
 /// @author concept
 /// @notice allows you to answer questions to win money
 contract HQ is Ownable {
     mapping(string => Question) public _quiz;
-    string[5] public questions;
+    string[] public questions;
     uint256 public _count = 0;
     
     struct Question {
         string prompt;
-        string answer;
+        bytes32 answer;
     }
 
     /// @notice a new question has been added
@@ -27,7 +27,9 @@ contract HQ is Ownable {
     /// @param _answer the answer that was given
     event CorrectAnswer(string _question, string _answer);
 
-    modifier notOnwer {
+    event WinnerChosen(address _winner);
+
+    modifier notOwner {
         require(msg.sender != owner(), "Owner cannot participate");
         _;
     }
@@ -47,7 +49,7 @@ contract HQ is Ownable {
 
     /// @notice the questions for this quiz
     /// @return the questions array
-    function getQuestions() external returns(string[] memory) {
+    function getQuestions() public view returns(string[] memory) {
         return questions;
     }
     
@@ -57,31 +59,35 @@ contract HQ is Ownable {
         require(getBalance() == 0, "Prize money has already been set");
         require(msg.value >= 1 ether, "Minimum amount is 1 ether");
     }
+    
 
     /// @notice how you answer the question
     /// @dev checks if your guess properly matches the question's answer
     /// @param _answer your answer to the question
     /// @param _question the question your curretly ansewring
-    function guess(string memory _question, string memory _answer) external view  notOnwer {
-        require(keccak256(abi.encodePacked(quiz[_question].answer)) == _answer, "Sorry your answer was incorrect");
+    function guess(string memory _question, string memory _answer) public notOwner {
+        require(_quiz[_question].answer == keccak256(abi.encodePacked(_answer)), "Sorry your answer was incorrect");
         emit CorrectAnswer(_question, _answer);
         _count++;
     }
 
     /// @notice collect your prize money
     /// @dev if you have answered all the questions correctly you can withdraw the contract balance
-    function withdraw() external notOnwer {
+    function withdraw() external notOwner {
         require(_count == questions.length, "Sorry you need to answer all the questions correctly");
         for(uint i = 0; i <= questions.length; i++) {
-           delete quiz[questions[i]]; 
+           delete _quiz[questions[i]]; 
         }
-        questions = [];
-        require(msg.sender.send(getBalance()));
+        delete questions;
+        (bool success, ) = payable(msg.sender).call{value: getBalance()}("");
+        require(success, "refund was unsuccessful");
+        assert(getBalance() == 0);
+        emit WinnerChosen(msg.sender);
     }
 
     /// @notice the prize money
     /// @return the current contract balance
-    function getBalance() external returns(uint256) {
+    function getBalance() public view returns(uint256) {
         return address(this).balance;
     }
 }
